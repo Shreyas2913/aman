1bb053b6d2aeb12511bbab2fe6755553a87ffe54
Shreyas2913 <100531024+Shreyas2913@users.noreply.github.com>
2022-03-06 15:38:27 +0530

    Create reddy.cpp
    Decision making

---
+++ reddy.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+using namespace std;
+int main()
+{
+int age;
+cout<<"Enter age of the voter:";
+cin>>age;
+if(age>=18)
+cout<<"You are eligible"<<endl;
+else
+cout<<"You are not eligible"<<endl;
+return 0;
+}
